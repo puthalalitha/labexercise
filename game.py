1bd40f36d5f5b0a4b3b77ed69d134acc9262c475
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-05 17:56:33 -0700

    lab-exercise guessing game

--- game.py
+++ game.py
@@ -6,16 +6,49 @@ name = input("What is your name? ")
 print("Hello, {}".format(name))
 print("{}, I'm thinking of a number between 1 and 100".format(name))
 print("Try to guess my number: ")
-random_num = random.randint(1, 100)
-count = 0
-while True:
-    guess = int(input("Your guess: "))
-    if guess != random_num:
+def guessing_game():
+    random_num = random.randint(1, 5)
+    count = 0
+    score = []
+    while True:
+        guess = input("Your guess between 1 - 100: ")
+
+        
+        if not guess.isdigit():
+            print("Enter  a number only ")
+            continue
+
+        guess = int(guess)
+
+        if guess not in range(1,5):
+
+            print("Enter a valid number: ")
+            continue
+       
+        elif guess != random_num:
+            
+            if guess > random_num:
+                print("Your number is too high, try again")
+
+            elif guess < random_num:
+                print("Your number is too low, try again")
+
+        elif guess == random_num:
+            print("Well done, {}! You found my number in {} tries.".format(name, count))
+            again = input("You want to play again?:")
+            score.append(count)
+            print(score)
+
+            if again.startswith("y"):
+                guessing_game()
+                
+                # score.append(count)
+                # print(score)
+                # count = 0
+
+            else:
+                break
+        
         count += 1
-        if guess > random_num:
-            print("Your number is too high, try again")
-        elif guess < random_num:
-            print("Your number is too low, try again")
-    else:
-        print("Well done, {}! You found my number in {} tries.".format(name, count))
-        break
+    # print(score)
+guessing_game()
